5f315c36745a155be89d0001c94c4217f9a80512
hazhyni <hazhiyahy4@gmail.com>
2022-03-16 09:09:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/smartdesa/DashboardActivity.java
@@ -0,0 +1,13 @@
+package com.example.smartdesa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_activity);
+    }
+}
---
+++ app/src/main/java/com/example/smartdesa/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.smartdesa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
